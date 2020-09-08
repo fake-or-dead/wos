d40ebb6aa7ebc71a6691cc3c771028bd19efbe76
PIYA PAKDEECHATURUN <a.postzone@gmail.com>
2020-09-08 16:38:35 +0700

    add list from post and page to index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,62 @@
-import React from "react"
-import { Link } from "gatsby"
+import React, { Component } from "react"
+import { Link, graphql } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-)
+class Homepage extends Component {
+  render() {
+    const data = this.props.data
 
-export default IndexPage
+    return (
+      <Layout>
+        <SEO title="HOME" />
+        <div>
+          <h1>Pages</h1>
+          {data.allWordpressPage.edges.map(({ node }) => (
+            <div key={node.slug}>
+              <Link to={node.slug}>
+                <h2>{node.title}</h2>
+              </Link>
+            </div>
+          ))}
+        </div>
+
+        <h1>Posts</h1>
+        {data.allWordpressPost.edges.map(({ node }) => (
+          <div key={node.slug}>
+            <Link to={node.slug}>
+              <h2>{node.title}</h2>
+            </Link>
+          </div>
+        ))}
+      </Layout>
+    )
+  }
+}
+
+export default Homepage
+
+export const pageQuery = graphql`
+  query {
+    allWordpressPage {
+      edges {
+        node {
+          id
+          title
+          excerpt
+          slug
+        }
+      }
+    }
+    allWordpressPost {
+      edges {
+        node {
+          title
+          excerpt
+          slug
+        }
+      }
+    }
+  }
+`
